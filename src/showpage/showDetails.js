9a861a3b4b3c606a347f22a152c8e40bb090d9a9
nihaprezz <serg13prez@outlook.com>
2020-05-17 22:05:13 -0400

    details styling done

--- src/showpage/showDetails.js
+++ src/showpage/showDetails.js
@@ -3,11 +3,13 @@ import React from 'react';
 function ShowDetails(props){
     let {borders, currencies, nativeName, flag, capital, population, region, subregion, topLevelDomain, languages, name } = props.country
 
-    return (
-        <div>
-                <div style={{backgroundImage: `url(${flag})`, width: 'auto', height: '10vw', backgroundSize: '100%'}}></div>
 
-                <h1>{name}</h1>    
+    return (
+        <div className='show-container'>
+            <div className='show-img' style={{backgroundImage: `url(${flag})`, width: 'auto', height: 'auto', backgroundSize: '100%'}}></div>
+            
+            <section className='details-container'>
+                <h1 className='show-name'>{name}</h1>    
 
                 <div className="details-1">
                     <p><span>Native Name: </span>{nativeName}</p>
@@ -21,14 +23,14 @@ function ShowDetails(props){
                     <p><span>Top Level Domain: </span>{topLevelDomain[0]}</p>
                     <p><span>Currencies: </span>{currencies[0].name}</p>
                     <p><span>Languages: </span>{languages.map(lang => lang.name).join(', ')}</p>
-
                 </div>
 
                 <div className="borders">
-                    <p>Border Countries: </p>
+                    <span>Border Countries: </span>
                     {borders.map((border, index) => <span key={index} className="bd">{border}</span>)}
                 </div>
-            </div>
+            </section>
+        </div>
     )
 }
 
--- src/showpage/showPage.js
+++ src/showpage/showPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import ShowDetails from './showDetails'
+import './show_page.css'
 
 const url = 'https://restcountries.eu/rest/v2'
 
@@ -20,7 +22,7 @@ const ShowPage = (props) => {
 
     return (
         <div>
-            <button>Back</button>
+            <Link to="/">Back</Link>
             {country && <ShowDetails country={country}/>}
         </div>
     )
