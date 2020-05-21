42f8c10db69df63e134b903294922a7697a0540f
nihaprezz <serg13prez@outlook.com>
2020-05-20 22:33:47 -0400

    added styling for search input

--- src/mainpage/filterForm.js
+++ src/mainpage/filterForm.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function FilterForm(props){
+    const [dropDown, setDropDown] = useState(false)
+
+
+
+    const filters = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+
     return (
         <div className="filter-form">
             <form onChange={(e) => props.updatingSearch(e)}>
-                <input placeholder="Search for Country"></input>
+                <ion-icon name="search-outline"></ion-icon>
+                <input placeholder="Search for a country.."></input>
             </form>
 
-            <form onChange={(e) => props.updateRegion(e)}>
-                <select defaultValue="">
-                    <option value="">Filter by Region</option>
-                    <option value='Africa'>Africa</option>
-                    <option value='Americas'>Americas</option>
-                    <option value='Asia'>Asia</option>
-                    <option value='Europe'>Europe</option>
-                    <option value='Oceania'>Oceania</option>
-                </select>
-            </form>
+            <div className="region-dd">
+                <p onClick={() => setDropDown(!dropDown)}>
+                    Filter By Region <ion-icon name="chevron-down-outline"></ion-icon>
+                </p>
+
+                <div>
+                    {dropDown && filters.map((region, index) => {
+                        return <li key={index}>{region}</li>
+                    })}
+               </div>
+            </div>
         </div>
     )
 }
